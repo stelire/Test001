225d0401249fce49904af4b3d41abe2916e4c776
stelire <s_naka@nippon-seiki.co.jp>
2018-08-28 20:03:20 +0900

    modify

--- Test001.cpp
+++ Test001.cpp
@@ -1,19 +1,33 @@
-//============================================================================
-// Name        : Test001.cpp
-// Author      : 
-// Version     :
-// Copyright   : Your copyright notice
-// Description : Hello World in C++, Ansi-style
-//============================================================================
+/*
+ * Test001.cpp
+ *
+ *  Created on: 2018/08/28
+ *      Author: stelire
+ */
 
 #include <iostream>
+#include "RDCPixel.h"
 using namespace std;
 
-int main(int argc, char **argv) {
-	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
-	cout << "!!!Second Message!!!" << endl;
+int main(int argc, char **argv)
+{
+	RDCPixel *pPx;
+	RDT_COLOR color = {0, 0, 0};
+
 	for(int i=0; i<argc; i++){
 		cout << argv[i] << endl;
 	}
+
+	pPx = new RDCPixel[20];
+
+	for(int i=0; i<20; i++){
+		pPx[i].setPos(0, 0);
+		pPx[i].setColor(color);
+	}
+
+	delete [] pPx;
+
 	return 0;
 }
+
+
